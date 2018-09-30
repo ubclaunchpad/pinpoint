8be6c645b0893e8415733474bbded7b881643b7d
Robert Lin <robertlin1@gmail.com>
2018-09-29 23:35:08 -0700

    Update configs to get tests reporting ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit
-          <pre>src/App.js</pre>
+          <code>src/App.js</code>
           and save to reload.
         </p>
       </div>
