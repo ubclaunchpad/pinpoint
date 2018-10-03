85bceb9a6b93ed872445972a2e669769cd0ae8f9
Tom Lee <win981026@gmail.com>
2018-10-02 21:38:58 -0700

    Setup SCSS for frontend
    - Renamed App.css and index.css to *.scss

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -150,6 +150,15 @@ module.exports = {
               cacheDirectory: true,
             },
           },
+          {
+            test: /\.scss$/,
+            include: paths.appSrc,
+            loaders: [
+              require.resolve('style-loader'),
+              require.resolve('css-loader'),
+              require.resolve('sass-loader'),
+            ]
+          },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
           // "style" loader turns CSS into JS modules that inject <style> tags.
@@ -197,7 +206,12 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [
+              /\.(js|jsx|mjs)$/,
+              /\.html$/,
+              /\.json$/,
+              /\.scss$/,
+            ],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
