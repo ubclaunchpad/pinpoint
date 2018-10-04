a402979ff0413d5a39df2271096667c5dfe85e32
Robert Lin <robertlin1@gmail.com>
2018-10-04 12:21:30 -0700

    Add grpc middleware to write activity to log

--- core/main.go
+++ core/main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/ubclaunchpad/pinpoint/core/service"
 	pinpoint "github.com/ubclaunchpad/pinpoint/grpc"
 	"github.com/ubclaunchpad/pinpoint/utils"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 )
 
 var (
@@ -38,25 +45,43 @@ func main() {
 			"error", err.Error())
 	}
 
-	// set up rpc server
-	var (
-		s    = grpc.NewServer()
-		addr = os.Getenv("CORE_HOST") + ":" + os.Getenv("CORE_PORT")
-	)
+	// Make sure that log statements internal to gRPC library are logged
+	grpcLogger := logger.Desugar().Named("grpc")
+	grpc_zap.ReplaceGrpcLogger(grpcLogger)
 
-	// spin up rpc server
-	pinpoint.RegisterPinpointCoreServer(s, core)
+	// Create a server, make sure we put the grpc_ctxtags context before everything else
+	opts := []grpc_zap.Option{
+		grpc_zap.WithDurationField(func(duration time.Duration) zapcore.Field {
+			return zap.Duration("grpc.duration", duration)
+		}),
+	}
+	grpcServer := grpc.NewServer(
+		grpc_middleware.WithUnaryServerChain(
+			grpc_ctxtags.UnaryServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
+			grpc_zap.UnaryServerInterceptor(grpcLogger, opts...)),
+		grpc_middleware.WithStreamServerChain(
+			grpc_ctxtags.StreamServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
+			grpc_zap.StreamServerInterceptor(grpcLogger, opts...)))
+
+	// Set address from environment
+	addr := os.Getenv("CORE_HOST") + ":" + os.Getenv("CORE_PORT")
+
+	// Prep grpc server
+	pinpoint.RegisterPinpointCoreServer(grpcServer, core)
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
-		logger.Fatalw("failed to start service",
+		logger.Fatalw("failed to start core service",
 			"error", err.Error())
 	}
-	logger.Infow("spinning up service",
+	logger.Infow("spinning up core service",
 		"host", os.Getenv("CORE_HOST"),
 		"core", os.Getenv("CORE_PORT"))
-	if err = s.Serve(listener); err != nil {
+
+	// Serve and block until exit
+	if err = grpcServer.Serve(listener); err != nil {
 		logger.Fatalw("error encountered",
 			"error", err.Error())
 	}
-	logger.Info("service shut down")
+
+	logger.Info("core service shut down")
 }
