ff96e0e465987fd524f84a0dc92934770e64ab7c
Mudasiru Abdurahman <w3q1b@ccs.ubc.ca>
2018-10-13 11:05:44 -0700

    installed react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.scss';
 
@@ -6,17 +7,30 @@ import './App.scss';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+              </header>
+              <p className="App-intro">
+                To get started, edit
+                <code>
+                  src/App.js
+                </code>
+                and save to reload.
+              </p>
+            </div>
+          </Route>
+          <Route exact path="/about">
+            <p>
+              Pinpoint is a versatile club application managment application
+            </p>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
