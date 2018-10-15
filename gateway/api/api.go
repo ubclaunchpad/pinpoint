a20f34735bd8387ea218a8b5bbdbd65d8553b6d2
Robert Lin <robertlin1@gmail.com>
2018-10-15 11:52:03 -0700

    Test gateway TLS startup, add API::Stop()

--- gateway/api/api.go
+++ gateway/api/api.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -18,9 +19,10 @@ import (
 // API defines the API server. It is primarily a REST interface through which
 // service.Service can be accessed.
 type API struct {
-	l *zap.SugaredLogger
-	r *chi.Mux
-	c pinpoint.CoreClient
+	l   *zap.SugaredLogger
+	r   *chi.Mux
+	c   pinpoint.CoreClient
+	srv *http.Server
 }
 
 // New creates a new API server - start it using Run(). Returns a callback to
@@ -101,33 +103,53 @@ func (a *API) Run(host, port string, opts RunOpts) error {
 	defer conn.Close()
 
 	// attempt connection
-	_, err = a.c.GetStatus(context.Background(), &request.Status{})
-	if err != nil {
-		return fmt.Errorf("failed to connect to core service: %s", err.Error())
+	go func() {
+		if _, err = a.c.GetStatus(context.Background(), &request.Status{}); err != nil {
+			a.l.Errorw("unable to connect to core service",
+				"error", err.Error())
+		} else {
+			a.l.Info("established connection to core")
+		}
+	}()
+
+	// set up server
+	a.srv = &http.Server{
+		Addr:    host + ":" + port,
+		Handler: a.r,
 	}
 
 	// lets gooooo
+	tlsEnabled := opts.GatewayOpts.CertFile != ""
 	a.l.Infow("spinning up api server",
 		"gateway.host", host,
 		"gateway.port", port,
-		"gateway.tls", opts.GatewayOpts.CertFile != "")
-	addr := host + ":" + port
-	if opts.GatewayOpts.CertFile != "" {
-		err = http.ListenAndServeTLS(
-			addr,
-			opts.GatewayOpts.CertFile,
-			opts.GatewayOpts.KeyFile,
-			a.r)
+		"gateway.tls", tlsEnabled)
+	if tlsEnabled {
+		if err := a.srv.ListenAndServeTLS(
+			opts.GatewayOpts.CertFile, opts.GatewayOpts.KeyFile,
+		); err != nil && err != http.ErrServerClosed {
+			a.l.Warnw("error encountered - service stopped",
+				"error", err)
+			return err
+		}
 	} else {
-		err = http.ListenAndServe(addr, a.r)
-	}
-	if err != nil {
-		a.l.Errorf("error encountered - service stopped",
-			"error", err)
-		return err
+		if err := a.srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			a.l.Warnw("error encountered - service stopped",
+				"error", err)
+			return err
+		}
 	}
 
 	// report shutdown
 	a.l.Info("service shut down")
 	return nil
+}
+
+// Stop shuts down the API server
+func (a *API) Stop() {
+	if a.srv != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		a.srv.Shutdown(ctx)
+		cancel()
+	}
 }
--- gateway/api/api_test.go
+++ gateway/api/api_test.go
@@ -30,4 +30,26 @@ func TestAPI_Run(t *testing.T) {
 	// stub run
 	go api.Run("localhost", "8080", RunOpts{})
 	time.Sleep(time.Millisecond)
+	api.Stop()
+
+	// stub run with core tls
+	go api.Run("localhost", "8080", RunOpts{
+		CoreOpts: CoreOpts{
+			Host:     "localhost",
+			Port:     "9111",
+			CertFile: "../../dev/certs/127.0.0.1.crt",
+		},
+	})
+	time.Sleep(time.Millisecond)
+	api.Stop()
+
+	// stub run with gateway tls
+	go api.Run("localhost", "8081", RunOpts{
+		GatewayOpts: GatewayOpts{
+			CertFile: "../../dev/certs/127.0.0.1.crt",
+			KeyFile:  "../../dev/certs/127.0.0.1.key",
+		},
+	})
+	time.Sleep(time.Millisecond)
+	api.Stop()
 }
