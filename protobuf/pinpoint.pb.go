937182bb28eb7a8692ddb7cf3866a46df15af769
Tom Lee <win981026@gmail.com>
2018-10-23 15:08:19 -0700

    Aded gRPC proto rpcs for Verify
    - Added Boolean response type
    - Added Verify request type
    - Added Verify rpc for pinpoint

--- protobuf/pinpoint.pb.go
+++ protobuf/pinpoint.pb.go
@@ -27,15 +27,16 @@ const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 func init() { proto.RegisterFile("pinpoint.proto", fileDescriptor_cbb17ad260bc57ce) }
 
 var fileDescriptor_cbb17ad260bc57ce = []byte{
-	// 114 bytes of a gzipped FileDescriptorProto
+	// 138 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x2b, 0xc8, 0xcc, 0x2b,
 	0xc8, 0xcf, 0xcc, 0x2b, 0xd1, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x80, 0xf1, 0xa5, 0x44,
 	0x8b, 0x52, 0x0b, 0x4b, 0x53, 0x8b, 0x4b, 0xf4, 0xa1, 0x34, 0x44, 0x81, 0x94, 0x78, 0x51, 0x6a,
-	0x71, 0x41, 0x7e, 0x5e, 0x71, 0xaa, 0x3e, 0x8c, 0x01, 0x91, 0x30, 0xb2, 0xe0, 0x62, 0x71, 0xce,
+	0x71, 0x41, 0x7e, 0x5e, 0x71, 0xaa, 0x3e, 0x8c, 0x01, 0x91, 0x30, 0x4a, 0xe5, 0x62, 0x71, 0xce,
 	0x2f, 0x4a, 0x15, 0x32, 0xe0, 0xe2, 0x74, 0x4f, 0x2d, 0x09, 0x2e, 0x49, 0x2c, 0x29, 0x2d, 0x16,
-	0xe2, 0xd7, 0x83, 0xe9, 0x86, 0x08, 0x48, 0x09, 0xe8, 0xc1, 0xb5, 0x41, 0x44, 0x94, 0x18, 0x92,
-	0xd8, 0xc0, 0x06, 0x18, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0x5c, 0xe0, 0x31, 0x3f, 0x8c, 0x00,
-	0x00, 0x00,
+	0xe2, 0xd7, 0x83, 0xe9, 0x86, 0x08, 0x48, 0x09, 0xe8, 0xc1, 0xb5, 0x41, 0x44, 0x94, 0x18, 0x84,
+	0xb4, 0xb9, 0xd8, 0xc2, 0x52, 0x8b, 0x32, 0xd3, 0x2a, 0x91, 0x94, 0x43, 0x04, 0xa4, 0xf8, 0x10,
+	0xca, 0x9d, 0xf2, 0xf3, 0x73, 0x94, 0x18, 0x92, 0xd8, 0xc0, 0xb6, 0x19, 0x03, 0x02, 0x00, 0x00,
+	0xff, 0xff, 0xd0, 0x28, 0x5f, 0x22, 0xb9, 0x00, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -51,6 +52,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type CoreClient interface {
 	GetStatus(ctx context.Context, in *request.Status, opts ...grpc.CallOption) (*response.Status, error)
+	Verify(ctx context.Context, in *request.Verify, opts ...grpc.CallOption) (*response.Bool, error)
 }
 
 type coreClient struct {
@@ -70,9 +72,19 @@ func (c *coreClient) GetStatus(ctx context.Context, in *request.Status, opts ...
 	return out, nil
 }
 
+func (c *coreClient) Verify(ctx context.Context, in *request.Verify, opts ...grpc.CallOption) (*response.Bool, error) {
+	out := new(response.Bool)
+	err := c.cc.Invoke(ctx, "/pinpoint.Core/Verify", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // CoreServer is the server API for Core service.
 type CoreServer interface {
 	GetStatus(context.Context, *request.Status) (*response.Status, error)
+	Verify(context.Context, *request.Verify) (*response.Bool, error)
 }
 
 func RegisterCoreServer(s *grpc.Server, srv CoreServer) {
@@ -97,6 +109,24 @@ func _Core_GetStatus_Handler(srv interface{}, ctx context.Context, dec func(inte
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Core_Verify_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(request.Verify)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CoreServer).Verify(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/pinpoint.Core/Verify",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CoreServer).Verify(ctx, req.(*request.Verify))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Core_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "pinpoint.Core",
 	HandlerType: (*CoreServer)(nil),
@@ -104,6 +134,10 @@ var _Core_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetStatus",
 			Handler:    _Core_GetStatus_Handler,
+		},
+		{
+			MethodName: "Verify",
+			Handler:    _Core_Verify_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
