682fc2524dabead48f4b2efb1cb5145e22a9d2cf
Srijon Saha <srijonsaha345@gmail.com>
2018-10-27 10:20:03 -0700

    Add tests for clubuser queries

--- core/database/database_test.go
+++ core/database/database_test.go
@@ -1,10 +1,10 @@
 package database
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/ubclaunchpad/pinpoint/core/model"
-
 	"github.com/ubclaunchpad/pinpoint/utils"
 )
 
@@ -32,27 +32,80 @@ func TestNew(t *testing.T) {
 	newTestDB(t)
 }
 
-func TestClubuser_Dao(t *testing.T) {
+func TestClubUser(t *testing.T) {
 	db := newTestDB(t)
-	u := &model.User{Email: "abc@def.com", Name: "Bob Ross", Salt: "qwer1234"}
+	u := &model.User{
+		Email: "abc@def.com",
+		Name:  "Bob Ross",
+		Salt:  "qwer1234",
+	}
 	c := &model.Club{
 		ID:          "1234",
 		Name:        "Launchpad",
 		Description: "1337 h4x0r",
-		Periods:     []string{"2018-2019"},
+		ClubTable:   "table-1234",
 	}
-	cu := &model.Clubuser{
+	cu := &model.ClubUser{
 		ClubID:   "1234",
 		Email:    "abc@def.com",
 		UserName: "Bob Ross",
 		Role:     "President",
 	}
-	db.AddNewUser(u)
-	res1, _ := db.GetUser(u.Email)
-	t.Log(*res1)
-	db.AddNewClub(c, cu)
-	res2, _ := db.GetClub(c.ID)
-	t.Log(*res2)
-	res3, _ := db.GetAllClubusers(c.ID)
-	t.Log(*res3)
+	cus := []*model.ClubUser{cu}
+
+	err := db.AddNewUser(u)
+	if err != nil {
+		t.Errorf("Failed to add new user: %s", err.Error())
+		t.FailNow()
+	}
+
+	userActual, err := db.GetUser(u.Email)
+	if err != nil {
+		t.Errorf("Failed to get user: %s", err.Error())
+		t.FailNow()
+	}
+	if !reflect.DeepEqual(u, userActual) {
+		t.Errorf("Failed to get expect user, expected: %+v, actual %+v", u, userActual)
+		t.FailNow()
+	}
+
+	err = db.AddNewClub(c, cu)
+	if err != nil {
+		t.Errorf("Failed to add new user: %s", err.Error())
+		t.FailNow()
+	}
+
+	clubActual, err := db.GetClub(c.ID)
+	if err != nil {
+		t.Errorf("Failed to add new user: %s", err.Error())
+		t.FailNow()
+	}
+	if !reflect.DeepEqual(c, clubActual) {
+		t.Errorf("Failed to get expect club, expected: %+v, actual %+v", c, clubActual)
+		t.FailNow()
+	}
+
+	clubUsersActual, err := db.GetAllClubUsers(c.ID)
+	if err != nil {
+		t.Errorf("Failed to add new user: %s", err.Error())
+		t.FailNow()
+	}
+	for i := range clubUsersActual {
+		if !reflect.DeepEqual(*cus[i], *clubUsersActual[i]) {
+			t.Errorf("Failed to get expect club, expected: %+v, actual %+v", *cus[i], *clubUsersActual[i])
+			t.FailNow()
+		}
+	}
+
+	err = db.DeleteClub(c.ID)
+	if err != nil {
+		t.Errorf("Failed to delete club %s", err.Error())
+		t.FailNow()
+	}
+
+	err = db.DeleteUser(u.Email)
+	if err != nil {
+		t.Errorf("Failed to delete user %s", err.Error())
+		t.FailNow()
+	}
 }
