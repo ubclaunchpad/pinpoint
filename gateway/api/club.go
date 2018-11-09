252cd4039c71e830659f46dfce4a2af52a7a92ee
chriscebrero <chris.cebrero@gmail.com>
2018-11-09 14:54:18 -0800

    created endpoint for club

--- gateway/api/club.go
+++ gateway/api/club.go
@@ -24,6 +24,7 @@ func newClubRouter(l *zap.SugaredLogger, c pinpoint.CoreClient) *ClubRouter {
 	router := chi.NewRouter()
 	club := &ClubRouter{l, c, router}
 	router.Post("/create_event", club.createEvent)
+	router.Post("/create_club", club.createClub)
 	return &ClubRouter{l.Named("clubs"), c, router}
 }
 
@@ -47,5 +48,24 @@ func (club *ClubRouter) createEvent(w http.ResponseWriter, r *http.Request) {
 
 	// TODO: create event in core
 
-	render.Render(w, r, res.Message(r, "Event created sucessfully", http.StatusCreated))
+	render.Render(w, r, res.Message(r, "Event created successfully", http.StatusCreated))
+}
+
+func (club *ClubRouter) createClub(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	// parse request data
+	var eData schema.CreateClub
+
+	if eData.Fields == nil {
+		render.Render(w, r, res.ErrBadRequest(r, errors.New("Missing fields"), "Missing fields"))
+	}
+
+	if err := decoder.Decode(&eData); err != nil {
+		render.Render(w, r, res.ErrBadRequest(r, err, "Invalid input"))
+		return
+	}
+
+	// TODO: create club in core
+
+	render.Render(w, r, res.Message(r, "Club created successfully", http.StatusCreated))
 }
---
+++ gateway/schema/club.go
@@ -0,0 +1,8 @@
+package schema
+
+//CreateClub defines a request to create a new club
+
+type CreateClub struct {
+	Name string
+	Desc string
+}
