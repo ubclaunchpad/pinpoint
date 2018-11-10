5680e1fd47eb8b6cc0eea17f7f9effba0d3fb0eb
klynx <KevinLynx_L@hotmail.com>
2018-11-10 15:03:54 -0800

    Added Signup page route

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -4,48 +4,47 @@ class Signup extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: '',
       email: '',
       password: '',
+      confirmpassword: '',
     };
     this.updateTextFields = this.updateTextFields.bind(this);
-    this.attemptLogin = this.attemptLogin.bind(this);
+    this.attemptSignup = this.attemptSignup.bind(this);
   }
 
   updateTextFields(e) {
+    const { name, confirmpassword } = this.state;
     const { email, password } = this.state;
-    console.log(email);
-    console.log(password);
-    const loginField = e.target.getAttribute('type');
-    this.setState({ [loginField]: e.target.value });
+    console.log(name, email, password, confirmpassword);
+    const field = e.target.getAttribute('type');
+    this.setState({ [field]: e.target.value });
   }
 
   // TODO once endpoint is set up, currently does nothing
-  attemptLogin(e) {
+  attemptSignup(e) {
     const { email, password } = this.state;
-    console.log(email);
-    console.log(password);
-    console.log(e);
+    console.log(email, password, e);
   }
 
   render() {
     return (
       <div className="flex-al-center">
-        <div className="title margin-title">Sign In</div>
+        <div className="title margin-title">Signup</div>
         <div className="flex-inlinegrid margin-top-xs margin-bottom-xs">
+          <input className="input-box input-small" type="name" placeholder="Name" onChange={this.updateTextFields} />
           <input className="input-box input-small" type="email" placeholder="Email" onChange={this.updateTextFields} />
           <input className="input-box input-small" type="password" placeholder="Password" onChange={this.updateTextFields} />
+          <input className="input-box input-small" type="confirmpassword" placeholder="Confirm Password" onChange={this.updateTextFields} />
         </div>
-        <div>
+        <div className="margin-top-xs">
           <input type="checkbox" />
-          <span>Remember me</span>
+          <span>Send me e-mail updates</span>
         </div>
-        <button className="click-button button-small animate-button margin-top-xs margin-bottom-xs" type="submit" onClick={this.attemptLogin}>Sign in</button>
-        <div className="loginhelp">
-          <a href="/login">Forgot Password?</a>
-        </div>
-        <div className="loginhelp">
-          <span>Don&#x2019;t have an account? &nbsp;</span>
-          <a href="/login">Sign up</a>
+        <button className="click-button button-small animate-button margin-top-xs margin-bottom-xs" type="submit" onClick={this.attemptSignup}>Sign up</button>
+        <div className="margin-top-xs">
+          <span>Already have a pinpoint account? &nbsp;</span>
+          <a href="/login">Sign In</a>
         </div>
       </div>
     );
