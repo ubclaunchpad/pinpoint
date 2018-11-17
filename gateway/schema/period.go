87a2ba0c8c731579a319e7a2dc52d70fc9bb2fc0
Susie <suujia@icloud.com>
2018-11-17 11:44:08 -0800

    Changed period schema

--- gateway/schema/period.go
+++ gateway/schema/period.go
@@ -1,7 +1,12 @@
 package schema
 
+import "time"
+
 // CreatePeriod defines a request to create an period
 type CreatePeriod struct {
-	Name   string
-	Fields []byte
+	Name       string
+	StartYear  int
+	StartMonth time.Month
+	EndYear    int
+	EndMonth   time.Month
 }
