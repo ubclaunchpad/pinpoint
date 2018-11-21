ee299af83c9d01aa6468a182bbe8a36cb405b497
Robert Lin <robertlin1@gmail.com>
2018-11-21 00:36:13 -0800

    Use json.RawMessage for events

--- gateway/schema/event.go
+++ gateway/schema/event.go
@@ -1,7 +1,15 @@
 package schema
 
-// CreateEvent : defines a request to create an event
+import "encoding/json"
+
+// CreateEvent defines a request to create an event
 type CreateEvent struct {
-	Name   string
-	Fields []byte
+	Name   string  `json:"name"`
+	Fields []Field `json:"fields"`
+}
+
+// Field defines a field of an event
+type Field struct {
+	Type       string          `json:"type"`
+	Properties json.RawMessage `json:"properties"`
 }
