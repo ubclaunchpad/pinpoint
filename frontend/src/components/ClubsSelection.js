f16faabb47dbf3f77ef81903cb8d7eb64b4ab1f5
Mudasiru Abdurahman <w3q1b@ccs.ubc.ca>
2018-12-05 19:43:12 -0800

    done

--- frontend/src/components/ClubsSelection.js
+++ frontend/src/components/ClubsSelection.js
@@ -15,21 +15,15 @@ class ClubsSelection extends Component {
 
   // TODO Replace unique key with database id of club
   generateclubs() {
-    console.log(this.state);
-    console.log('test');
-    console.log();
-
     const { clubs } = this.state;
-    const clublist = clubs.map((club) => (
+    return clubs.map((club) => (
       <li className="margin-left-s margin-right-s" key={Math.random() * 10000}>
-        <img className="club-img-l" src={club.imagePath} alt={club.name} />
+        <img className="club-img-l" src={require('../assets/ubclaunchpad-logo.png')} alt={club.name} />
         <p className="textwrap">
           {club.name}
         </p>
       </li>
     ));
-
-    return <ul className="flex-inline margin-top-xs margin-bottom-xs">{ clublist }</ul>;
   }
 
   render() {
@@ -38,9 +32,11 @@ class ClubsSelection extends Component {
         <div className="title margin-title"> Your Clubs </div>
         <ul className="flex-inline margin-top-xs margin-bottom-xs">
           {this.generateclubs()}
-          <li>
-            <img className="club-img-l pad-left-m" src={require('../assets/newclub.png')} alt="new club" />
-            <p> Create a new club </p>
+          <li className="margin-left-s margin-right-s" key={Math.random() * 10000}>
+            <img className="club-img-l" src={require('../assets/newclub.png')} alt="new club" />
+            <p className="textwrap">
+              Create new club
+            </p>
           </li>
         </ul>
       </div>
