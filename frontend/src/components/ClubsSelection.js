bef5b7997f7fc0b093599f53219de49d2acff7a1
Mudasiru Abdurahman <w3q1b@ccs.ubc.ca>
2018-12-07 14:52:51 -0800

    Removed unused class from spacing.scss

--- frontend/src/components/ClubsSelection.js
+++ frontend/src/components/ClubsSelection.js
@@ -10,37 +10,52 @@ class ClubsSelection extends Component {
 
   // Mock loading of database object into component
   componentDidMount() {
-    this.setState({ clubs: [{ name: 'UBC Launchpad 1', imagePath: '../assets/ubclaunchpad-logo.png' }, { name: 'UBC Launchpad 2', imagePath: '../assets/ubclaunchpad-logo.png' }] });
+    this.setState({
+      clubs: [
+        {
+          name: 'UBC Launchpad 1',
+          imagePath: '../assets/ubclaunchpad-logo.png'
+        },
+        {
+          name: 'UBC Launchpad 2',
+          imagePath: '../assets/ubclaunchpad-logo.png'
+        }
+      ]
+    });
   }
 
   // TODO Replace unique key with database id of club
   generateclubs() {
-<<<<<<< HEAD
-=======
     console.log(this.state);
->>>>>>> 815f46f034c074b70b77bef94c55ea0163bdffb5
     const { clubs } = this.state;
-    return clubs.map((club) => (
-      <li className="margin-left-s margin-right-s" key={Math.random() * 10000}>
-        <img className="club-img-l" src={require('../assets/ubclaunchpad-logo.png')} alt={club.name} />
-        <p className="textwrap">
-          {club.name}
-        </p>
+    return clubs.map(club => (
+      <li className='margin-left-s margin-right-s' key={Math.random() * 10000}>
+        <img
+          className='club-img-l'
+          src={require('../assets/ubclaunchpad-logo.png')}
+          alt={club.name}
+        />
+        <p className='textwrap'>{club.name}</p>
       </li>
     ));
   }
 
   render() {
     return (
-      <div className="flex-al-center">
-        <div className="title margin-title"> Your Clubs </div>
-        <ul className="flex-inline margin-top-xs margin-bottom-xs">
+      <div className='flex-al-cente'>
+        <div className='title margin-title'> Your Clubs </div>
+        <ul className='flex-inline margin-top-xs margin-bottom-xs'>
           {this.generateclubs()}
-          <li className="margin-left-s margin-right-s" key={Math.random() * 10000}>
-            <img className="club-img-l" src={require('../assets/newclub.png')} alt="new club" />
-            <p className="textwrap">
-              Create new club
-            </p>
+          <li
+            className='margin-left-s margin-right-s'
+            key={Math.random() * 10000}
+          >
+            <img
+              className='club-img-l'
+              src={require('../assets/newclub.png')}
+              alt='new club'
+            />
+            <p className='textwrap'>Create new club</p>
           </li>
         </ul>
       </div>
