e63deca6ab21c5fd6ab27fbbdf221e832eb881bb
Tom Lee <win981026@gmail.com>
2019-02-19 23:34:41 -0800

    Start work on abstracting color/icon

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 class Notification extends Component {
+	constructor(props, context) {
+		super(props, context);
+	}
+
+	getColorClass() {
+		let colors = {
+			info: 'blue',
+			success: 'green',
+			warning: 'orange',
+			error: 'red',
+		};
+		return colors[this.props.type];
+	}
+
+	getIconClass() {
+		let icons = {
+			info: 'fa-info-circle',
+			success: 'fa-check',
+			warning: 'fa-warning',
+			error: 'fa-times-circle',
+		};
+		return icons[this.props.type];
+	}
+
 	render() {
 		if (this.props.showNotification) {
 			return (
-				<div className="notification">
-					<p>this.props.message</p>
+				<div className={`pad-ends-xs highlight-${getColorClass()}`}>
+					<i className={`fa ${this.getIconClass()`} />
+					{this.props.message}
 				</div>
 			);
 		}
