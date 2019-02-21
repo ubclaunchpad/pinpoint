d8e4bc96224f8bc0c9e80ecd8dc26ba2112ea77f
Tom Lee <win981026@gmail.com>
2019-02-20 20:29:04 -0800

    Remove unused value

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -20,7 +20,6 @@ class Login extends Component {
   }
 
   updateTextField(e) {
-    const { notification } = this.state;
     const loginField = e.target.getAttribute('name');
     this.setState({
       notification: null,
