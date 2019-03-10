5867d3dc0e0087369978c9cf61af35824999e0d2
Robert Lin <robertlin1@gmail.com>
2019-03-09 23:31:14 -0800

    Use test logger for newTestDB

--- core/database/database_test.go
+++ core/database/database_test.go
@@ -2,8 +2,29 @@ package database
 
 import (
 	"testing"
+
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/ubclaunchpad/pinpoint/utils"
+	"go.uber.org/zap/zaptest"
 )
 
+// newTestDB is a utility function to create a connection to a local dynamodb
+// instance and expose the underlying client for test purposes
+func newTestDB(t *testing.T) (*Database, *dynamodb.DynamoDB) {
+	s, err := utils.AWSSession(utils.AWSConfig(true))
+	if err != nil {
+		return nil, nil
+	}
+
+	// attempt to create database with connection
+	db, err := New(s, zaptest.NewLogger(t).Sugar())
+	if err != nil {
+		return nil, nil
+	}
+
+	return db, db.c
+}
+
 func TestNew(t *testing.T) {
-	NewTestDB()
+	newTestDB(t)
 }
