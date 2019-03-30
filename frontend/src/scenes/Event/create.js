2d9b1a6496730455b44b43e3477e4ac4bf0e00e9
Susie <suujia@icloud.com>
2019-03-30 00:36:43 -0700

    remove unnecessary functions:

--- frontend/src/scenes/Event/create.js
+++ frontend/src/scenes/Event/create.js
@@ -1,42 +1,9 @@
 import React, { Component } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 
 class CreateEvent extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      startDate: new Date(),
-      endDate: new Date(),
-      hasError: false,
-    };
     this.updateTextFields = this.updateTextFields.bind(this);
-    this.handleChangeStart = this.handleChangeStart.bind(this);
-    this.handleChangeEnd = this.handleChangeEnd.bind(this);
-  }
-
-  handleChangeStart(date) {
-    const { endDate } = this.state;
-    if (endDate < date) {
-      this.setState({ hasError: true });
-      const { hasError } = this.state;
-      console.log('Start date cannot be greater than end date!', hasError);
-    } else {
-      this.setState({
-        startDate: date,
-      });
-    }
-  }
-
-  handleChangeEnd(date) {
-    const { startDate } = this.state;
-    if (startDate > date) {
-      this.setState({ hasError: true });
-      const { hasError } = this.state;
-      console.log('Start date cannot be greater than end date!', hasError);
-    } else {
-      this.setState({ endDate: date });
-    }
   }
 
   updateTextFields(e) {
