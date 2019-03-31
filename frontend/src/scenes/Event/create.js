194e6edcc9b358ab654f5bd90d78dc48a3fc70f7
Susie <suujia@icloud.com>
2019-03-30 22:32:23 -0700

    more like a card evet frontend page

--- frontend/src/scenes/Event/create.js
+++ frontend/src/scenes/Event/create.js
@@ -14,13 +14,15 @@ class CreateEvent extends Component {
   render() {
     return (
       <div>
-        <div className="flex dir-col pad-left-300">
-          <div className="title margin-title">Create Event</div>
-          <h2 className="fw-normal">Add a point of data entry to your application period</h2>
-          <h2 className="flex-ai-start pad-top-xxl fw-normal">Event Name </h2>
+        <div className="flex-al-center card margin-top-100 margin-sides-auto w-800">
+          <div className="title card-title">Create Event</div>
+          <h2 className="fw-normal card-description">Add a point of data entry to your application period</h2>
+          <h2 className="flex-ai-start pad-top-xxl fw-normal card-text">Event Name</h2>
           <div className="flex-inlinegrid margin-top-xs margin-bottom-xs">
             <input className="input-box input-large" id="applications" placeholder="Eg. Launch Pad Interview Notes" onChange={this.updatetextfields} />
           </div>
+          <div className="card-button margin-top-xs">Next</div>
+          <h2 className="fw-normal card-description">Cancel</h2>
         </div>
       </div>
     );
